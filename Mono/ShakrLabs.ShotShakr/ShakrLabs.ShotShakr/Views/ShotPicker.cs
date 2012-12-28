9a10dfa99135eb8077b20439eef6d26cc0670872
Matt Krebs <matt.krebs@outlook.com>
2012-12-28 02:53:15 -0600

    updates

--- Mono/ShakrLabs.ShotShakr/ShakrLabs.ShotShakr/Views/ShotPicker.cs
+++ Mono/ShakrLabs.ShotShakr/ShakrLabs.ShotShakr/Views/ShotPicker.cs
@@ -11,11 +11,12 @@ using Android.Views;
 using Android.Widget;
 using Android.Graphics;
 using Android.Views.Animations;
+using Android.Hardware;
 
 namespace ShakrLabs.ShotShakr
 {
     [Activity(Label = "Shot Shakr")]
-    public class ShotPicker : Activity, ShakrLabs.ShotShakr.ShakeEventListener.OnShakeListener
+    public class ShotPicker : Activity, ISensorEventListener
     {
         private const int SHAKE_THRESHOLD = 500;
         private ViewFlipper mFlipper;
@@ -28,7 +29,7 @@ namespace ShakrLabs.ShotShakr
         private TextView shotDetailName;
         private TextView shotDetailIngred;
 
-      
+        private SensorManager _shakeListener;
 
         private ImageButton btnTookThis;
 
@@ -60,13 +61,16 @@ namespace ShakrLabs.ShotShakr
         List<String> filters = new List<String>();
         private GestureDetector gestureScanner;
 
-  
+
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.shot_picker);
             // Create your application here
+            _shakeListener = (SensorManager)GetSystemService(Context.SensorService);
+         
+
 
             // /set Global Animations
             left_out = AnimationUtils.LoadAnimation(this, Resource.Animation.push_left_out);
@@ -78,42 +82,42 @@ namespace ShakrLabs.ShotShakr
             mFlipper = ((ViewFlipper)this.FindViewById<ViewFlipper>(Resource.Id.flipper));
 
             mFlipper.SetInAnimation(this, Resource.Animation.push_left_in);
-            mFlipper.SetOutAnimation(this,Resource.Animation.push_left_out);
-           
-            vibe = (Vibrator) GetSystemService(Context.VibratorService);
+            mFlipper.SetOutAnimation(this, Resource.Animation.push_left_out);
+
+            vibe = (Vibrator)GetSystemService(Context.VibratorService);
 
 
-            shotName =  FindViewById<ShotNameTextView>(Resource.Id.shot);
+            shotName = FindViewById<ShotNameTextView>(Resource.Id.shot);
             shotName2 = FindViewById<ShotNameTextView>(Resource.Id.shot_2);
 
             shotDetailName = FindViewById<TextView>(Resource.Id.shotName);
             shotDetailIngred = FindViewById<TextView>(Resource.Id.shotIngredients);
 
-            mFlipper.Touch +=mFlipper_Touch;
+            mFlipper.Touch += mFlipper_Touch;
 
-            fontFace = Typeface.CreateFromAsset(Assets,"Fonts/homework normal.TTF");
+            fontFace = Typeface.CreateFromAsset(Assets, "Fonts/homework normal.TTF");
 
             homescene = true;
 
             shotName.SetTypeface(fontFace, TypefaceStyle.Normal);
 
             shotName.Text = "Shake to get this party started!";
-            
+
 
 
 
 
 
             View inflatedDrawerLayout = LayoutInflater.Inflate(Resource.Layout.filterdrawer, null);
-          
+
             int width = Window.Attributes.Width;
             int height = Window.Attributes.Height;
-            
+
             Android.Widget.LinearLayout.LayoutParams param = new Android.Widget.LinearLayout.LayoutParams(width, height);
             Window.AddContentView(inflatedDrawerLayout, param);
 
 
-           
+
 
 
         }
@@ -121,8 +125,16 @@ namespace ShakrLabs.ShotShakr
         {
             base.OnResume();
 
+            _shakeListener.RegisterListener(this, _shakeListener.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Ui);
             presenter = ShotPresenter.Current;
             List<Shot> shots = presenter.Shots;
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+
         }
 
         void mFlipper_Touch(object sender, View.TouchEventArgs e)
@@ -153,6 +165,123 @@ namespace ShakrLabs.ShotShakr
 
         }
 
+
+        #region Shake Listener
+
+        /** Minimum movement force to consider. */
+        private const int MIN_FORCE = 10;
+
+        /**
+         * Minimum times in a shake gesture that the direction of movement needs to
+         * change.
+         */
+        private const int MIN_DIRECTION_CHANGE = 3;
+
+        /** Maximum pause between movements. */
+        private const int MAX_PAUSE_BETHWEEN_DIRECTION_CHANGE = 500;
+
+        /** Maximum allowed time for shake gesture. */
+        private const int MAX_TOTAL_DURATION_OF_SHAKE = 400;
+
+        /** Time when the gesture started. */
+        private long mFirstDirectionChangeTime = 0;
+
+        /** Time when the last movement started. */
+        private long mLastDirectionChangeTime;
+
+        /** How many movements are considered so far. */
+        private int mDirectionChangeCount = 0;
+
+        /** The last x position. */
+        private float lastX = 0;
+
+        /** The last y position. */
+        private float lastY = 0;
+
+        /** The last z position. */
+        private float lastZ = 0;
+
        
+
+     
+        private void resetShakeParameters()
+        {
+            mFirstDirectionChangeTime = 0;
+            mDirectionChangeCount = 0;
+            mLastDirectionChangeTime = 0;
+            lastX = 0;
+            lastY = 0;
+            lastZ = 0;
+        }
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+        {
+
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            Console.WriteLine("Sensor Changed");
+            // get sensor data
+            float x = e.Values[1];
+            float y = e.Values[2];
+            float z = e.Values[0];
+
+            // calculate movement
+            float totalMovement = System.Math.Abs(x + y + z - lastX - lastY - lastZ);
+
+            if (totalMovement > MIN_FORCE)
+            {
+
+                // get time
+                long now = CurrentTimeMillis();
+
+                // store first movement time
+                if (mFirstDirectionChangeTime == 0)
+                {
+                    mFirstDirectionChangeTime = now;
+                    mLastDirectionChangeTime = now;
+                }
+
+                // check if the last movement was not long ago
+                long lastChangeWasAgo = now - mLastDirectionChangeTime;
+                if (lastChangeWasAgo < MAX_PAUSE_BETHWEEN_DIRECTION_CHANGE)
+                {
+
+                    // store movement data
+                    mLastDirectionChangeTime = now;
+                    mDirectionChangeCount++;
+
+                    // store last sensor data 
+                    lastX = x;
+                    lastY = y;
+                    lastZ = z;
+
+                    // check how many movements are so far
+                    if (mDirectionChangeCount >= MIN_DIRECTION_CHANGE)
+                    {
+
+                        // check total duration
+                        long totalDuration = now - mFirstDirectionChangeTime;
+                        if (totalDuration < MAX_TOTAL_DURATION_OF_SHAKE)
+                        {
+                            onShake();
+                            resetShakeParameters();
+                        }
+                    }
+
+                }
+                else
+                {
+                    resetShakeParameters();
+                }
+            }
+        }
+        private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public long CurrentTimeMillis()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+        #endregion
     }
 }
--- Mono/ShakrLabs.ShotShakr/ShakrLabs.ShotShakr/obj/Debug/android/src/shakrlabs/shotshakr/ShotPicker.java
+++ Mono/ShakrLabs.ShotShakr/ShakrLabs.ShotShakr/obj/Debug/android/src/shakrlabs/shotshakr/ShotPicker.java
@@ -4,13 +4,17 @@ package shakrlabs.shotshakr;
 public class ShotPicker
 	extends android.app.Activity
 	implements
-		mono.android.IGCUserPeer
+		mono.android.IGCUserPeer,
+		android.hardware.SensorEventListener
 {
 	static final String __md_methods;
 	static {
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
 			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onAccuracyChanged:(Landroid/hardware/Sensor;I)V:GetOnAccuracyChanged_Landroid_hardware_Sensor_IHandler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onSensorChanged:(Landroid/hardware/SensorEvent;)V:GetOnSensorChanged_Landroid_hardware_SensorEvent_Handler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
 			"";
 		mono.android.Runtime.register ("ShakrLabs.ShotShakr.ShotPicker, ShakrLabs.ShotShakr, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ShotPicker.class, __md_methods);
 	}
@@ -38,6 +42,30 @@ public class ShotPicker
 	}
 
 	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onAccuracyChanged (android.hardware.Sensor p0, int p1)
+	{
+		n_onAccuracyChanged (p0, p1);
+	}
+
+	private native void n_onAccuracyChanged (android.hardware.Sensor p0, int p1);
+
+
+	public void onSensorChanged (android.hardware.SensorEvent p0)
+	{
+		n_onSensorChanged (p0);
+	}
+
+	private native void n_onSensorChanged (android.hardware.SensorEvent p0);
 
 	java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
